47d76fc79485afaee1e142ae40bec75ada9cdb12
AssiaVerona <90841939+AssiaVerona@users.noreply.github.com>
2021-10-15 13:21:43 +0300

    Update 9 (4).cpp

--- задание к теме 9/9 (4).cpp
+++ задание к теме 9/9 (4).cpp
@@ -1,21 +1,21 @@
 ﻿// 9 (4).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	int A, B, C, S1, S2, k;
+	int A, B, C, S1, S2, k, d, r1, r2;
 	cout << " Enter the values: A, B, C " << endl;
 	cin >> A >> B >> C;
 	if ((A > 0) and (B > 0) and (C > 0))
 	{
-		S1 = A * B;
-		S2 = C * C;
-		k = S1 / S2;
-		cout << " Number of squares = " << k;
+		r1 = A/C;
+		r2 = B/C;
+		S1 = r1 * r2;
+		S2 = A * B - C*C*S1;
+		cout << " Number of squares = " << S1 << endl << "unoccupied area of ​​the rectangle = " << S2;
 	}
 	else
 	{
